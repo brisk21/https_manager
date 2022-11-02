9c68cb94e6d2def7ce404ae1f25d449203098797
琅玡-琪拉 <1076963452@qq.com>
2022-11-02 16:37:46 +0800

    mysql显示当前数据库大小

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -4,6 +4,8 @@
 namespace app\admin\controller;
 
 
+use think\Db;
+
 class Index extends Base
 {
     public function index()
@@ -21,9 +23,12 @@ class Index extends Base
             'logEmailSuc' => model('logEmail')->where('status', '=', 1)->count(),
             'logEmailError' => model('logEmail')->where('status', '=', -1)->count('try_count'),
         ];
-        $data['dbSize'] = 'mysql不支持';
+
         if (config('database.type') == 'sqlite') {
             $data['dbSize'] = round(filesize(str_replace('sqlite:', '', config('database.dsn'))) / 1024 / 1024, 2);
+        } else {
+            $query = Db::query(" select round(sum(DATA_LENGTH/1024/1024),2) as data from information_schema.TABLES where TABLE_SCHEMA ='" . (config('database.database')) . "' LIMIT 1;");
+            $data['dbSize'] = !empty($query[0]['data']) ? $query[0]['data'] : '0.00';
         }
 
         $this->assign('data', $data);
